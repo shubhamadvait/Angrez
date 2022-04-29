238e8928d946cd1060d380e1d67a5f00086cd738
shubham <lionshubham1995@gmail.com>
2022-04-29 14:51:49 +0530

    Data has  been send

--- Maven_test/src/main/java/qaclickacademy/RESTAPITest.java
+++ Maven_test/src/main/java/qaclickacademy/RESTAPITest.java
@@ -8,6 +8,9 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postjira");
+		System.out.println("hurajira");
+		System.out.println("nirajira");
+		System.out.println("pirajira");
 	}
 	@Test
 	public void deleteTwitter()
